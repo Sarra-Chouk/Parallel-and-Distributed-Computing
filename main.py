bdfbd10f584e353783838fb142ef317a3d5b68c0
Sarra Chouk <6030037@udst.edu.qa>
2025-03-19 11:28:29 +0300

    Add concurrency

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from src.tasks import power
 from src.dispatch_tasks import dispatch
+import time
 
 if __name__ == "__main__":
+    start_time = time.time()
     results = dispatch()
+    end_time = time.time()
     print(results[:10])
+    print(f"Total time taken: {end_time - start_time:.2f} seconds")
